c281c57b9c68ba2b9d3220055f609e213cc7ccad
sohailul <sohailulalam@gmail.com>
2022-04-03 23:21:30 +0600

    project setup and routes created

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>This is Dashboard for Charts</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
